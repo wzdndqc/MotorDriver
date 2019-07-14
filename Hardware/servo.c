290c6ca451d61896efdd670cea36f5210d3cc2ed
Tamap <tamapchn@outlook.com>
2019-07-14 19:58:04 +0800

    更新Servo的函数和函数注释

--- Hardware/servo.c
+++ Hardware/servo.c
@@ -1,23 +1,8 @@
 /*
-Servo Driver
-
-Hardware res:
-
-T1_1 : PA8
-T1_2 : PA9
-T1_3 : PA10
-T1_4 : PA11
-T8_1 : PC6
-T8_2 : PC7
-T8_3 : PC8
-T8_4 : PC9
-
-Software res:
-
-T1 : TIM1
-T8 : TIM8
-
-GPIOA GPIOB GPIOC GPIOD
+ * @file	servo.c
+ * @auther	Tamap
+ * @brief	Servo Driver
+ * @date	2019-7-14
 */
 
 //Inc
@@ -26,7 +11,7 @@ GPIOA GPIOB GPIOC GPIOD
 //Var
 
 //Fun
-void Servo_Config(void)
+void Servo_ConfigT8(void)
 {
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 	TIM_OCInitTypeDef TIM_OCInitStructure;
@@ -71,4 +56,51 @@ void Servo_Config(void)
 	TIM_ARRPreloadConfig(TIM8, ENABLE);
 	TIM_Cmd(TIM8, ENABLE);
 	TIM_CtrlPWMOutputs(TIM8, ENABLE);
+}
+
+void Servo_ConfigT1(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	//RCC
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+	RCC_APB2PeriphClockCmd(
+		RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO,
+		ENABLE);
+
+	//GPIO
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	//TIM Base : 1us Psc 20ms Per
+	TIM_TimeBaseStructure.TIM_Period = 20000 - 1;
+	TIM_TimeBaseStructure.TIM_Prescaler = (SystemCoreClock / 1000000) - 1;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+	//TIM OC
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+	TIM_OCInitStructure.TIM_Pulse = 1500;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_Low;
+	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
+	TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
+	TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+	TIM_OC2Init(TIM1, &TIM_OCInitStructure);
+	TIM_OC3Init(TIM1, &TIM_OCInitStructure);
+	TIM_OC4Init(TIM1, &TIM_OCInitStructure);
+	TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+	TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
+	TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
+	TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
+	TIM_ARRPreloadConfig(TIM1, ENABLE);
+	TIM_Cmd(TIM1, ENABLE);
+	TIM_CtrlPWMOutputs(TIM1, ENABLE);
 }
--- Hardware/servo.h
+++ Hardware/servo.h
@@ -1,23 +1,23 @@
 /*
-Servo Driver
-
-IO res:
-
-T1_1 : PA8
-T1_2 : PA9
-T1_3 : PA10
-T1_4 : PA11
-T8_1 : PC6
-T8_2 : PC7
-T8_3 : PC8
-T8_4 : PC9
-
-Hardware res:
-
-T1 : TIM1
-T8 : TIM8
-
-GPIOA GPIOC
+ * @file	servo.h
+ * @auther	Tamap
+ * @brief	Servo Driver
+ * @date	2019-7-14
+ * 
+ * IO res:
+ * T1_1 : PA8
+ * T1_2 : PA9
+ * T1_3 : PA10
+ * T1_4 : PA11
+ * T8_1 : PC6
+ * T8_2 : PC7
+ * T8_3 : PC8
+ * T8_4 : PC9
+ * 
+ * Hardware res:
+ * T1 : TIM1
+ * T8 : TIM8
+ * GPIOA GPIOC
 */
 #ifndef _SERVO_H_
 #define _SERVO_H_
@@ -28,7 +28,33 @@ GPIOA GPIOC
 //Var
 
 //Fun
-void Servo_Config(void);
+/*
+ * @brief Init the defaults Servo interface.
+ */
+#define Servo_Config Servo_ConfigT8
+/*
+ * @brief Init the Servo interface at PC6~PC9 with TIM8.
+ */
+void Servo_ConfigT8(void);
+/*
+ * @brief Init the Servo interface at PA8~PA11 with TIM1.
+ */
+void Servo_ConfigT1(void);
+
+/*
+ * @brief Get/Set output of servo channel
+ * 
+ * @param [in] tim	*TIM_Typedef:
+ * 	-TIM8
+ * 	-TIM1
+ * @param [in] ch	Channel of pwm output:
+ * 	-1~4
+ * @return The channel
+ * @par	Example:
+ * @code
+ * 	Servo_Ch(TIM8,1) = 1500; // 1.5ms
+ * @endcode
+ */
 #define Servo_Ch(tim,ch) (tim->CCR##ch)
 
 #endif
